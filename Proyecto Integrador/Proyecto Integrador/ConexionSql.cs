3f9095dec7e72b5321a45ff70fa9b2658f84925c
Lox4ever <andresbellas2004@gmail.com>
2025-06-06 13:40:17 -0300

    Acceso a datos + img modelo de clases

---
+++ Proyecto Integrador/Proyecto Integrador/ConexionSql.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logica.Logica
+{
+    public class ConexionSql
+    {
+        private SqlConnection conexion = new SqlConnection(); // instanciamos la conexion a la base
+        private SqlCommand comando = new SqlCommand();  // instanciamos el comando para ejecutar las query
+        private SqlDataReader lector; // No se instancia
+
+        public SqlDataReader Lector
+        {
+            get { return lector; } // Generamos el get para leer desde afuera el lector ya que es private
+        }
+
+        //conexion a la base
+        public ConexionSql()
+        {
+            try
+            {
+                conexion = new SqlConnection(@"Server=.\SQLEXPRESS;Database=PROMOS_DB;Integrated Security=True");
+                //Conexion para base de datos con declaracion de usuario especifico con login de sql
+               //conexion.ConnectionString = "Server=ULARIAGA-BRAIAN\\LOCALHOST; Database= PROMOS_DB; User Id= sa; Password=Super123.adm "; /// Brian
+               
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+            }
+        }
+
+        //Metodo para la consulta
+        public void Consulta(string consulta)
+        {
+            try
+            {
+            comando.CommandType = System.Data.CommandType.Text; // Tipo de comando a ajecutar en el SQL, text(query), storeproceduire(SP)
+            comando.CommandText = consulta; //Pasamos la consulta por parametro
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        //Metodo para ejecutar consulta
+        public void Ejecutar()
+        {
+
+            comando.Connection = conexion; //Nos conectamos
+
+            try
+            {
+                conexion.Open(); // abrimos conexion
+                lector = comando.ExecuteReader(); // Leemos el reusltado del query
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+        public void EjecutarAccion()
+        {
+
+            comando.Connection = conexion; //Nos conectamos
+
+            try
+            {
+                conexion.Open(); // abrimos conexion
+                comando.ExecuteReader(); // Leemos el reusltado del query sin guardarlo
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        public void cerrarConexion()
+        {
+
+            try
+            {
+
+                if (lector != null)
+                {
+                    lector.Close();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (conexion != null && conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                }
+
+            }
+        }
+
+            public void SetParametros(string nombre, object valor)
+            {
+                comando.Parameters.AddWithValue(nombre, valor);
+            }
+
+        
+        }
+    }
+
